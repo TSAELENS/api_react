99e426af3a1acb5efd7b8ce4fed86a504ef1a23c
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-07 14:06:37 +0200

    Post

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,29 +1,80 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { tokensContext } from "../provider";
 
 const Post = () => {
 	const [state, dispatch] = useContext(tokensContext);
 
-	return (
+	const [formData, setFormData] = useState({
+		title: "",
+		body: ""
+	  });
+
+	  const handleChange = (e) => {
+		setFormData({
+		  ...formData,
+		  [e.target.name]: e.target.value
+		});
+	  };
+
+	  const handleSubmit = async (e) => {
+		e.preventDefault();
+	  
+		try {
+		  const response = await fetch("https://example.com/api/posts", {
+			method: "POST",
+			headers: {
+			  "Content-Type": "application/json"
+			},
+			body: JSON.stringify(formData)
+		  });
+	  
+		  if (response.ok) {
+			// La requête POST a réussi
+			console.log("Publication réussie !");
+			// Réinitialiser le formulaire
+			setFormData({
+			  title: "",
+			  body: ""
+			});
+		  } else {
+			// La requête POST a échoué
+			console.log("Erreur lors de la publication.");
+		  }
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+	  
+	  
+	  return (
 		<>
-			<h1>Post Page</h1>
-
-			{/* <section>
-				<h2>Posts</h2>
-
-				{state.blogPosts.map((post, index) => (
-					<article key={index} className="post">
-						<h3>{post.title}</h3>
-						<div className="img-wrapper">
-							<img src={`img/${post.image}`} alt={post.title} />
-						</div>
-						<p>{post.description}</p>
-						<p>{post.date}</p>
-					</article>
-				))}
-			</section> */}
+		  <h1>Post Page</h1>
+	  
+		  <form onSubmit={handleSubmit}>
+			<label>
+			  Title:
+			  <input
+				type="text"
+				name="title"
+				value={formData.title}
+				onChange={handleChange}
+			  />
+			</label>
+			<br />
+			<label>
+			  Body:
+			  <textarea
+				name="body"
+				value={formData.body}
+				onChange={handleChange}
+			  ></textarea>
+			</label>
+			<br />
+			<button type="submit">Submit</button>
+		  </form>
 		</>
-	);
+	  );
+	  
 };
 
 export default Post;
