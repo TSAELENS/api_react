634d9524e4c1b408623a8c7c87b767a68087ab04
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-07 14:10:46 +0200

    Auth

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,29 +1,55 @@
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { tokensContext } from "../provider";
 
 const Auth = () => {
 	const [state, dispatch] = useContext(tokensContext);
+	const [username, setUsername] = useState('');
+	const [password, setPassword] = useState('');
 
-	return (
+	const handleUsernameChange = (e) => {
+		setUsername(e.target.value);
+	  };
+	  
+	  const handlePasswordChange = (e) => {
+		setPassword(e.target.value);
+	  };
+
+	  const handleLogin = (e) => {
+		e.preventDefault();
+	  
+		// Effectuez ici la logique d'authentification en utilisant les valeurs de `username` et `password`
+	  
+		// Exemple de requête d'authentification
+		if (username === 'admin' && password === 'password') {
+		  // Authentification réussie, mettez à jour le contexte avec les informations d'authentification
+		  dispatch({ type: 'LOGIN', payload: { username } });
+		} else {
+		  // Authentification échouée, affichez un message d'erreur ou effectuez d'autres actions
+		  console.log('Identifiant ou mot de passe incorrect');
+		}
+	  };
+
+	  
+	  return (
 		<>
-			<h1>Auth Page</h1>
-
-			{/* <section>
-				<h2>Posts</h2>
-
-				{state.blogPosts.map((post, index) => (
-					<article key={index} className="post">
-						<h3>{post.title}</h3>
-						<div className="img-wrapper">
-							<img src={`img/${post.image}`} alt={post.title} />
-						</div>
-						<p>{post.description}</p>
-						<p>{post.date}</p>
-					</article>
-				))}
-			</section> */}
+		  <h1>Login</h1>
+	  
+		  <form onSubmit={handleLogin}>
+			<label>
+			  Username:
+			  <input type="text" value={username} onChange={handleUsernameChange} />
+			</label>
+			<br />
+			<label>
+			  Password:
+			  <input type="password" value={password} onChange={handlePasswordChange} />
+			</label>
+			<br />
+			<button type="submit">Login</button>
+		  </form>
 		</>
-	);
+	  );
+	  
 };
 
 export default Auth;
