56a8c5bb2c456d7dbe73dbe90e1447e11cdebfc1
TSAELENS <77736077+TSAELENS@users.noreply.github.com>
2023-06-07 14:34:16 +0200

    Details home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,18 +4,28 @@ import { tokensContext } from "../provider";
 const Home = () => {
   const [state, dispatch] = useContext(tokensContext);
 
-  const [people, setPeople] = useState([]);
+  const [characterDetails, setCharacterDetails] = useState([]);
+
 
   useEffect(() => {
-    const fetchPeople = async () => {
-      try {
-        const response = await fetch('https://swapi.dev/api/people/');
-        const data = await response.json();
-        setPeople(data.results);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+	const fetchPeople = async () => {
+		try {
+		  const response = await fetch('https://swapi.dev/api/people/');
+		  const data = await response.json();
+	  
+		  // Récupérer les détails des personnages individuels
+		  const characterDetailsPromises = data.results.map(async (person) => {
+			const response = await fetch(person.url);
+			return response.json();
+		  });
+	  
+		  const characterDetailsData = await Promise.all(characterDetailsPromises);
+		  setCharacterDetails(characterDetailsData);
+		} catch (error) {
+		  console.error(error);
+		}
+	  };
+	  
 
     fetchPeople();
   }, []);
@@ -27,9 +37,15 @@ const Home = () => {
       <div>
         <h1>Characters from Star Wars</h1>
         <ul>
-          {people.map((person, index) => (
-            <li key={index}>{person.name}</li>
-          ))}
+		{characterDetails.map((character, index) => (
+			<div key={index}>
+				<h2>{character.name}</h2>
+				<p>Height: {character.height}</p>
+				<p>Mass: {character.mass}</p>
+				<p>Hair Color: {character.hair_color}</p>
+				{/* Afficher d'autres informations souhaitées */}
+			</div>
+			))}
         </ul>
       </div>
     </>
